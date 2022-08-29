ce8b69f08bf56938d3ea69740bc62fc26b31d9b2
genildon-barreto <88632877+genildon-barreto@users.noreply.github.com>
2022-08-28 23:49:21 -0300

    Create banco.sql
    Banco de dados criado para o desafio do curso instruído pelo professor Denilson Bonatti

---
+++ Docker: Utilização Prática no Cenário de Microsserviços/Desafio/banco.sql
@@ -0,0 +1,17 @@
+CREATE TABLE dados (
+    AlunoID int,
+    Nome varchar(50),
+    Sobrenome varchar(50),
+    Endereco varchar(150),
+    Cidade varchar(50),
+    Host varchar(50)
+);
+
+CREATE TABLE dados (
+    ProfessorID int,
+    Nome varchar(50),
+    Sobrenome varchar(50),
+    Endereco varchar(150),
+    Cidade varchar(50),
+    Host varchar(50)
+);
