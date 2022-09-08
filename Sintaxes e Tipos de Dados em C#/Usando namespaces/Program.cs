0d63bbaf4f6e7a7f1ff2987a737593dd738ad3a7
genildon-barreto <88632877+genildon-barreto@users.noreply.github.com>
2022-09-08 04:32:52 -0700

    Update Program.cs
    Edição efetuada com base na aula de declarações de tipos de variáveis.

--- Sintaxes e Tipos de Dados em C#/Usando namespaces/Program.cs
+++ Sintaxes e Tipos de Dados em C#/Usando namespaces/Program.cs
@@ -1,7 +1,20 @@
-﻿using Usando_namespaces.Models;
+using Usando_namespaces.Models;
 
-Pessoa pessoinha1 = new Pessoa();
+string apresentacao = "Olá, seja bem-vindo!";
+int quantidade = 1;
+double altura = 1.80;
+decimal preco = 1.80M;
+bool condicao = true;
 
-pessoinha1.Nome = "Barreto";
-pessoinha1.Idade = 27;
-pessoinha1.Apresentar();
+
+Console.WriteLine(apresentacao);
+Console.WriteLine("Valor da variável quantidade: " + quantidade);
+Console.WriteLine("Valor da variável quantidade: " + altura.ToString("0.00"));
+Console.WriteLine("Valor da variável quantidade: " + preco);
+Console.WriteLine("Valor da variável quantidade: " + condicao);
+
+//Pessoa pessoinha1 = new Pessoa();
+
+//pessoinha1.Nome = "Barreto";
+//pessoinha1.Idade = 27;
+//pessoinha1.Apresentar();
