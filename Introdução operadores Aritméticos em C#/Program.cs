b7275f47d60205ff18e2119a73d038f11f87e73a
genildon-barreto <88632877+genildon-barreto@users.noreply.github.com>
2022-09-10 04:31:14 -0700

    Projeto realizado conforme acompanhamento de aulas sobre operadores aritméticos do professsor Leonardo Buta.

---
+++ Introdução operadores Aritméticos em C#/Program.cs
@@ -0,0 +1,50 @@
+﻿using _3__Introdução_operadores_Aritméticos.Models;
+
+// <---- Aula - Criando nossa classe Calculadora ---->
+
+Calculadora calc = new Calculadora();
+
+// calc.Somar(10, 30);
+// calc.Subtrair(10, 50);
+// calc.Multiplicar(15, 45);
+// calc.Divisão(2, 2);
+
+
+
+// <---- Aula - Usando potência ---->
+
+// calc.Potencia(3, 3);
+
+
+
+// <---- Aula - Usando potência ---->
+
+// calc.Seno(30);
+// calc.Coseno(30);
+// calc.Tangente(30);
+
+
+
+// <---- Aula - Incremento e Decremento ---->
+
+int numeroIncremento = 10;
+
+Console.WriteLine(numeroIncremento);
+Console.WriteLine("Incrementando o 10");
+// numero = numero + 1;
+// numeroIncremento++;
+// Console.WriteLine(numeroIncremento);
+
+// int numeroDecremento = 20;
+
+// Console.WriteLine(numeroDecremento);
+// Console.WriteLine("Decrementando o 20");
+// // numero = numero - 1;
+// numeroDecremento--;
+// Console.WriteLine(numeroDecremento);
+
+
+
+// <---- Aula - Calculando Raiz Quadrada ---->
+
+calc.RaizQuadrada(9);
